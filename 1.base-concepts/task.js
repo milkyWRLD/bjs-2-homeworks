b8a7dc24ac58ef96cae93bb486d6b58373ae7854
artsid <90769533+milkyWRLD@users.noreply.github.com>
2023-09-09 00:38:39 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,26 +1,26 @@
 "use strict";
 
 function solveEquation(a, b, c) {
+  
+  const discriminant = b ** 2 - 4 * a * c;
 
-	const discriminant = b ** 2 - 4 * a * c;
-
-	а
-	if (discriminant < 0) {
-
-		return [];
-	} else if (discriminant === 0) {
-
-		const root = -b / (2 * a);
-		return [root];
-	} else {
-
-		const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-		const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-		return [root1, root2];
-	}
+  
+  if (discriminant < 0) {
+    
+    return [];
+  } else if (discriminant === 0) {
+    
+    const root = -b / (2 * a);
+    return [root];
+  } else {
+    
+    const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [root1, root2];
+  }
 }
 
-
+// Пример использования функции
 const a = 1;
 const b = -3;
 const c = 2;
