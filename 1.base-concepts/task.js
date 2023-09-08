d32047e79f200d4b497576a986588730353e803c
artsid <90769533+milkyWRLD@users.noreply.github.com>
2023-09-09 00:44:40 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,31 +1,37 @@
 "use strict";
 
 function solveEquation(a, b, c) {
+  let arr = [];
   
-  const discriminant = b ** 2 - 4 * a * c;
+  const discriminant = b * b - 4 * a * c;
 
-  
   if (discriminant < 0) {
-    
-    return [];
+
+    return arr;
   } else if (discriminant === 0) {
-    
+
     const root = -b / (2 * a);
-    return [root];
+    arr.push(root);
   } else {
-    
+
     const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
     const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    return [root1, root2];
+    arr.push(root1, root2);
   }
+
+  return arr;
 }
 
-// Пример использования функции
-const a = 1;
-const b = -3;
-const c = 2;
-const roots = solveEquation(a, b, c);
-console.log("Корни уравнения:", roots);
+const roots1 = solveEquation(1, -3, 2); // Два корня: [2, 1]
+const roots2 = solveEquation(1, 2, 1);  // Один корень: [-1]
+const roots3 = solveEquation(1, 1, 1);  // Нет корней: []
+
+console.log("Корни уравнения 1: " + roots1);
+console.log("Корни уравнения 2: " + roots2);
+console.log("Корни уравнения 3: " + roots3);
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+}
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     percent = parseFloat(percent);
