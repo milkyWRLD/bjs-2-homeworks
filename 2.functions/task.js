d3ece94ef5cbf02800ca4e1754a6223539e5532c
Web_artsid <web.dev.artsid@gmail.com>
2023-09-12 20:39:31 +0300

    Второе задание полностью готово
    Took 51 minutes

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,120 @@
+// Задача 1: Функция getArrayParams
 function getArrayParams(...arr) {
+  if (arr.length === 0) {
+    return { min: undefined, max: undefined, avg: undefined };
+  }
 
-  return { min: min, max: max, avg: avg };
+  let min = Infinity; 
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  const avg = parseFloat((sum / arr.length).toFixed(2));
+
+  return { min, max, avg };
 }
 
+// Задача 2: Функции summElementsWorker, differenceMaxMinWorker, differenceEvenOddWorker, averageEvenElementsWorker
 function summElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0; // Если массив пустой, возвращаем 0
+  }
 
+  return arr.reduce((sum, num) => sum + num, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) {
+    return 0; // Если массив пустой, возвращаем 0
+  }
 
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+
+  if (countEvenElement === 0) {
+    return 0; // Если нет четных элементов, возвращаем 0
+  }
+
+  return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
+// Задача 3: Функция makeWork
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
 
+  for (const arr of arrOfArr) {
+    const result = func(...arr);
+
+    if (result > maxWorkerResult) {
+      maxWorkerResult = result;
+    }
+  }
+
+  return maxWorkerResult;
 }
+
+const arr = [
+  [10, 10, 11, 20, 10],
+  [67, 10, 2, 39, 88],
+  [72, 75, 51, 87, 43],
+  [30, 41, 55, 96, 62]
+];
+
+console.log(getArrayParams(-99, 99, 10));
+console.log(getArrayParams(1, 2, 3, -100, 10));
+console.log(getArrayParams(5));
+
+console.log(summElementsWorker());
+console.log(summElementsWorker(10, 10, 11, 20, 10));
+
+console.log(differenceMaxMinWorker()); // 0
+console.log(differenceMaxMinWorker(10, 10, 11, 20, 10));
+
+console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17));
+console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
+
+console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9));
+console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
+
+console.log(makeWork(arr, summElementsWorker));
+console.log(makeWork(arr, differenceMaxMinWorker));
+console.log(makeWork(arr, differenceEvenOddWorker));
+console.log(makeWork(arr, averageEvenElementsWorker));
