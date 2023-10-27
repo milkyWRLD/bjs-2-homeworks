e676d768515cc92048c803015b81fff1a5a07e52
Web_artsid <web.dev.artsid@gmail.com>
2023-10-27 23:24:55 +0300

    Второе задание полностью готово!
    Took 34 minutes

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.subject = "";
+    this.marks = [];
+    this.excluded = "";
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+    this.subject = subjectName || undefined;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (!this.excluded) {
+        this.marks.push(...marksToAdd);
+    }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    } else {
+        const sum = this.marks.reduce((total, mark) => total + mark, 0);
+        return sum / this.marks.length;
+    }
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+    this.subject = undefined;
+    this.marks = undefined;
+    this.excluded = reason;
+};
